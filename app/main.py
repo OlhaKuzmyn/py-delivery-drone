e9e0ade4b49fb8c9ed25f802ea14104a7f796fa1
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-16 16:35:36 +0200

    current_load default = None

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ class DeliveryDrone(FlyingRobot):
             name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo | None,
+            current_load: Cargo | None = None,
             coords: list | tuple = (0, 0, 0)
     ) -> None:
         super().__init__(name, weight, coords)
